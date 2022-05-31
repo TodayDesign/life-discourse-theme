a1fd76711e70bf967450901b78d6035cc26df53a
Lucas Mounsey <lucas@today.design>
2022-05-31 16:05:34 +1000

    Fix button

--- javascripts/discourse/api-initializers/life.js
+++ javascripts/discourse/api-initializers/life.js
@@ -3,3 +3,10 @@ import { apiInitializer } from "discourse/lib/api";
 export default apiInitializer("0.11.1", api => {
   console.log("hello world from api initializer!");
 });
+
+
+// Update the font variable
+document.documentElement.style.setProperty('--heading-font-family', `'Poppins', Arial, Helvetica, sans-serif`);
+
+// Highlight medium
+document.documentElement.style.setProperty('--highlight-medium', '#F3F3F3');
